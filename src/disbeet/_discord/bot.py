be76d1772e91b46d39e06e5217215ed81c7cddd7
Griff <rickleg.93@gmail.com>
2024-11-16 23:32:36 -0500

    Bot can send messages now. Moved to single configuration. Now need to work on getting the state message in the channel. Maybe a pinned message would be easier than finding the first message from the bot user? This message will be used to keep updates about current directories in the new imports dir. need to also figure out how to refresh this method. Thinking about a user creating a thread containing the name of a folder they want imported. then bot can grab name of the folder and guide user through import process

--- src/disbeet/_discord/bot.py
+++ src/disbeet/_discord/bot.py
@@ -1,19 +1,28 @@
+import abc
 import json
 from pathlib import Path
-from typing import Any, Dict
+from typing import Any, Dict, Optional, Sequence, cast
 
-import discord
-from pydantic import BaseModel
+from discord import Client, Intents, Message, TextChannel
+from pydantic import BaseModel, field_validator
 
 
 class BotConfig(BaseModel):
     app_id: str
     public_key: str
     token: str
+    guild_id: int
     import_channel_id: int
+    beets_config: Path
+    new_imports_dir: Path
+
+    @field_validator("beets_config", "new_imports_dir", mode="before")
+    @classmethod
+    def to_pathlike(cls, v: str) -> Path:
+        return Path(v)
 
 
-class Bot(discord.Client):
+class Bot(Client):
     def __init__(self, config_filepath: str):
         self._intents = self.initialize_intents()
         config_path = Path(config_filepath)
@@ -38,11 +47,11 @@ class Bot(discord.Client):
 
         # TODO: Why does this not like when the type of config_dict is Dict[str, Union[str, int]]?
         self.config: BotConfig = BotConfig(**config_dict)
-
+        self.state_message = self.get_state_message()
         super().__init__(intents=self._intents)
 
-    def initialize_intents(self) -> discord.Intents:
-        intents = discord.Intents.default()
+    def initialize_intents(self) -> Intents:
+        intents = Intents.default()
         intents.message_content = True
         return intents
 
@@ -50,11 +59,51 @@ class Bot(discord.Client):
         super().run(self.config.token)
 
     async def on_ready(self) -> None:
+        await self.publish_filesystem_udpate()
         print("bot ready!")
 
-    async def on_message(self, message: discord.Message) -> None:
+    async def on_message(self, message: Message) -> None:
         if message.channel.id == self.config.import_channel_id:
             await self.process_message(message)
 
-    async def process_message(self, message: discord.Message) -> None:
+    async def process_message(self, message: Message) -> None:
         print(f"Received message: {message.author}: {message.content}")
+
+    async def publish_filesystem_udpate(self) -> None:
+        # new_import_subdirs = await self.get_filesystem_listing()
+        # state_message = await self.get_state_message()
+        channel = await self.get_import_channel()
+        await channel.send("hello")
+
+    async def get_import_channel(self) -> TextChannel:
+        my_guild = self.guilds[0]
+        if my_guild is None:
+            raise RuntimeError(f"Could not find server with id {self.config.guild_id}")
+
+        channel = my_guild.get_channel(self.config.import_channel_id)
+        if not channel:
+            raise RuntimeError(
+                f"Could not find channel with id {self.config.import_channel_id}"
+            )
+
+        if not isinstance(channel, TextChannel):
+            raise RuntimeError(
+                f"Channel with given ID is not a text channel {self.config.import_channel_id}"
+            )
+
+        return channel
+
+    async def get_state_message(self) -> Optional[Message]:
+        return None
+        # return await discord.utils.get(
+        #     self.get_guild(self.config.guild_id)
+        #     .get_channel(self.config.import_channel_id)
+        #     .history(limit=1, oldest_first=True)
+        # )
+
+    async def get_filesystem_listing(self) -> Sequence[Path]:
+        return [
+            directory
+            for directory in self.config.new_imports_dir.iterdir()
+            if directory.is_dir()
+        ]
--- src/disbeet/main.py
+++ src/disbeet/main.py
@@ -10,14 +10,9 @@ def main() -> None:
         description="A discord bot used for managing a beets library through discord",
     )
 
-    parser.add_argument("--import-dir", "-i", dest="new_imports_directory")
-    parser.add_argument("--beets-config", "-b", dest="beets_config")
-    parser.add_argument("--discord-config", "-d", dest="discord_config", required=True)
+    parser.add_argument("--config", "-c", dest="config", required=True)
 
     args = parser.parse_args()
-    print(f"New Imports Directory: {args.new_imports_directory}")
-    print(f"Beets config configuration: {args.beets_config}")
-    print(f"Discord config filepath: {args.discord_config}")
 
-    discord_bot = Bot(args.discord_config)
+    discord_bot = Bot(args.config)
     discord_bot.start_disbeet_bot()
